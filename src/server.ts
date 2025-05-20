5a31d373278142aa28c657f1156d1ad71e783404
sadinal04 <sadinal@mhs.usk.ac.id>
2025-05-20 17:26:34 +0700

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./utils/db";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
@@ -10,18 +12,24 @@ import challengeRoutes from "./routes/challengeRoutes";
 import publicChallengeRoutes from "./routes/publicChallengeRoutes";
 import submissionRoutes from "./routes/submissionRoutes";
 
-dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const mongoURI = process.env.MONGO_URI;
+console.log("MONGO_URI:", mongoURI);
+
+if (!mongoURI) {
+  console.error("Error: MONGO_URI environment variable is not set!");
+  process.exit(1);
+}
+
+connectDB(mongoURI);
 
 app.use(cors());
 
 // limit size
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
-
-connectDB(process.env.MONGO_URI!);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
